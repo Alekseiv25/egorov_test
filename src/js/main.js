3f88738a57e89f627948ce78da474de3fa3e3c22
Alekseiv25 <aleskeiv25@gmail.com>
2023-05-19 17:54:21 +0300

    add: main task

--- src/js/main.js
+++ src/js/main.js
@@ -21,6 +21,70 @@ let x = setInterval(function () {
         document.getElementById("hours").innerText = "";
         document.getElementById("minutes").innerText = "";
         document.getElementById("seconds").innerText = "";
-        document.getElementById("expired").innerText = "Время истекло";
     }
 }, 1000);
+
+
+
+//Updates text
+let elementsToUpdate = [
+    { element: document.getElementById('daystext'), fullText: 'Days', shortText: 'DD' },
+    { element: document.getElementById('hourstext'), fullText: 'Hours', shortText: 'HH' },
+    { element: document.getElementById('minutestext'), fullText: 'Minutes', shortText: 'MM' },
+    { element: document.getElementById('secondstext'), fullText: 'Seconds', shortText: 'SS' }
+];
+
+function updateText() {
+    let windowWidth = window.innerWidth;
+
+    elementsToUpdate.forEach(function (elementToUpdate) {
+        if (windowWidth < 768) {
+            elementToUpdate.element.innerText = elementToUpdate.shortText;
+        } else {
+            elementToUpdate.element.innerText = elementToUpdate.fullText;
+        }
+    });
+}
+
+updateText();
+
+window.addEventListener('resize', updateText);
+
+
+document.getElementById('subscribeForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    let email = document.getElementById('email').value;
+
+    if (!validateEmail(email)) {
+        alert("Введите действительный адрес электронной почты.");
+        return;
+    }
+    const url = "https://run.mocky.io/v3/44d7016a-d7fd-4c2e-b29d-45a1684ef34d"
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("POST", url);
+
+    xmlhttp.onreadystatechange = function () {
+        let popup = document.getElementById('popup');
+        let message = document.getElementById('message');
+        let title_message = document.getElementById('title_message')
+
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            title_message.innerText = 'success!'
+            message.innerText = "You have successfully subscribed to the email newsletter";
+            popup.style.display = 'block';
+        } else if (xmlhttp.readyState == 4) {
+            title_message.innerText = 'error'
+            message.innerText = "Something went wrong. Please try again.";
+            popup.style.display = 'block';
+        }
+    }
+
+    xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xmlhttp.send("email=" + encodeURIComponent(email));
+});
+
+function validateEmail(email) {
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
